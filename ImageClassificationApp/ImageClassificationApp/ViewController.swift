d493f4186ca9024feecf163b4a649344ee61cd3b
Jaehoon Lee <wannabewize@gmail.com>
2022-11-28 06:18:10 +0900

    결과 업데이트

--- ImageClassificationApp/ImageClassificationApp/ViewController.swift
+++ ImageClassificationApp/ImageClassificationApp/ViewController.swift
@@ -11,19 +11,34 @@ import Vision
 class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var resultView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let url = Bundle.main.url(forResource: "image2", withExtension: "png") else {
+        guard let url = Bundle.main.url(forResource: "image1", withExtension: "jpeg") else {
             fatalError("image url")
         }
         
-        let handler = VNImageRequestHandler(url: url, options: [:])
         let request = VNClassifyImageRequest()
+        print("Count :", (try! request.supportedIdentifiers().count))
+        let handler = VNImageRequestHandler(url: url)
         try? handler.perform([request])
         
-        if let observations = request.results {
-            print("Observation :", observations)
+        if let observations : [VNClassificationObservation] = request.results {
+            
+            let filtered = observations
+            .filter { observation in
+                observation.confidence > 0.5
+            }
+            .sorted { lhs, rhs in
+                lhs.confidence > rhs.confidence
+            }
+            .map { observation in
+                "\(observation.identifier) - \(observation.confidence)"
+            }
+            print("Observation :", filtered)
+            
         }
         else {
             print("no result :", request.results)
